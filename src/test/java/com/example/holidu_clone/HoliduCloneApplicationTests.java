5d7f9c8d54cb21bf645e4892fcfa3464dd671b3f
mohamed.kadi <n.kadi63@gmail.com>
2024-09-16 16:01:55 -0400

    Initial Spring Boot setup for Holidu clone

---
+++ src/test/java/com/example/holidu_clone/HoliduCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holidu_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoliduCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
